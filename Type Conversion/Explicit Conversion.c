2eb1defe3a2a3a7accbbe16942a06363ed85e050
Riya Hal <riyahal.dev@gmail.com>
2025-03-01 18:25:02 +0530

    Add files via upload

---
+++ Type Conversion/Explicit Conversion.c
@@ -0,0 +1,12 @@
+// Explicit Type Conversion
+
+#include<stdio.h>
+int main() {
+    float res;
+    int num = 10;
+    res = (float)num/3; // Converting int to float
+    printf("Result: %f\n", res);
+    return 0;
+}
+
+// Output: Result: 3.333333
---
+++ Type Conversion/Implicit Type Conversion.c
@@ -0,0 +1,13 @@
+// Implicit Type Conversion
+
+#include<stdio.h>
+int main() {
+    float var = 20;
+    char ch = 'B';
+    float res;
+    res = ch + var;
+    printf("Result: %f\n", res);
+    return 0;
+}
+
+// Output: Result: 86.000000

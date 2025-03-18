978b11bad6f9789d8cc5a8b063fe96163292a77a
Riya Hal <riyahal.dev@gmail.com>
2025-03-18 20:12:12 +0530

    Add files via upload

---
+++ Looping Statements/for loop.c
@@ -0,0 +1,27 @@
+// for loop
+
+#include<stdio.h>
+void main(){
+    for (int i = 0; i <= 15; i++){
+        printf("%d\n", i);
+    }
+}
+
+/* Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+*/
